103cfe35ae3bb6b9c31fc8b539f0d88116266378
nisha-shah <nisha.shah2208@gmail.com>
2013-06-06 10:28:09 -0700

    Initial Commit

---
+++ VolunteerApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VolunteerApp
+//
+//  Created by Nisha Shah on 6/6/13.
+//  Copyright (c) 2013 Nisha Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VolunteerApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VolunteerApp
+//
+//  Created by Nisha Shah on 6/6/13.
+//  Copyright (c) 2013 Nisha Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
